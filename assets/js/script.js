218709f4f2331b11cee126ad224ad138e0647fa4
Danish <danish.wajid786@gmail.com>
2020-03-15 00:25:49 +0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  var stepMove = false;
+  $("button[btn-next]").click(function() {
+    const currentStep = $("#steps-container").attr("step");
+    $("#steps-container").attr("step", parseInt(currentStep) + 1);
+    $("#step-" + currentStep).addClass("d-none");
+    $("#step-" + (parseInt(currentStep) + 1)).removeClass("d-none");
+
+    if (stepMove) {
+      $("#barStep-" + currentStep).removeClass("active");
+      $("#barStep-" + (parseInt(currentStep) + 1)).addClass("active");
+    } else {
+      $("#barStep-" + currentStep).addClass("d-none");
+      $("#barStep-" + currentStep).removeClass("active");
+      $("#barStep-" + (parseInt(currentStep) + 1)).addClass("active");
+      $("#barStep-" + (parseInt(currentStep) + 5)).removeClass("d-none");
+    }
+
+    if (currentStep == 39) {
+      $("button[btn-next]").addClass("d-none");
+      $("a[btn-submit]").removeClass("d-none");
+    }
+    if (currentStep == 34) {
+      $("#barStep-extention").addClass("d-none");
+      stepMove = true;
+    }
+  });
+});
