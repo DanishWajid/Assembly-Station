04806cbbdd6b5516e275292c972114c2b774f863
Danish <danish.wajid786@gmail.com>
2020-03-27 20:48:05 +0500

    new functionalities updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,56 @@
 $(document).ready(function() {
-  for (var step = 0; step < 40; step++) {
-    $("#step-" + step).prop("checked", false);
+  for (var step = 0; step < 400; step++) {
+    $("#step-" + step + " input").prop("checked", false);
   }
   var lang = localStorage.getItem("language");
   var count = 0;
   var scroll = 0;
   $("button[btn-next]").click(function() {
     const currentStep = $("#steps-container").attr("step");
+    const currentList = $("#steps-container").attr("list");
     $("#steps-container").attr("step", parseInt(currentStep) + 1);
-    $("#scrollView").scrollTop(scroll);
-    $("#step-" + currentStep).prop("checked", true);
-    $("#step-" + currentStep).attr("disabled", true);
+    $("#step-" + (parseInt(currentStep) + 1)).removeClass("d-none");
 
-    if (lang == "de") {
-      $("button[btn-next]").text("Nächster Arbeitsschritt");
-    } else if (lang == "en") {
-      $("button[btn-next]").text("Next Working-Step");
+    if ($("#step-" + currentStep).next("ul").length == 0) {
+      if ($("#step-" + currentStep).next().length == 0) {
+        $("#step-" + (parseInt(currentStep) - 3) + " input").prop(
+          "checked",
+          true
+        );
+        $("#step-" + currentStep + " input").prop("checked", true);
+      } else {
+        $("#step-" + currentStep + " input").prop("checked", true);
+      }
     }
 
-    scroll += 30;
     count += 1;
-    if (count == 9 && currentStep != 99) {
+    if (count == 39 && currentStep != 399) {
+      $("button[btn-next]").css("background-color", "green");
       if (lang == "de") {
         $("button[btn-next]").text("Nächstes Arbeitspaket");
-      } else if (lang == "en") {
+      } else {
         $("button[btn-next]").text("Next Working-Package");
       }
-    } else if (count == 10 && currentStep != 100) {
-      $("#barStep-" + (parseInt(currentStep) - 9)).removeClass("active");
-      $("#barStep-" + (parseInt(currentStep) + 1)).addClass("active");
+    }
+    if (count == 40 && currentStep != 400) {
+      $("#list-" + currentList).addClass("d-none");
+      $("#steps-container").attr("list", parseInt(currentList) + 1);
+      $("button[btn-next]").css("background-color", "#007bff");
       if (lang == "de") {
         $("button[btn-next]").text("Nächster Arbeitsschritt");
-      } else if (lang == "en") {
+      } else {
         $("button[btn-next]").text("Next Working-Step");
       }
+      $("#barStep-" + currentList).removeClass("active");
+      $("#barStep-" + (parseInt(currentList) + 1)).addClass("active");
       count = 0;
+      scroll = 0;
     }
 
-    if (currentStep == 99) {
+    $("#scrollView").scrollTop(scroll);
+    scroll += 30;
+
+    if (currentStep == 399) {
       $("button[btn-next]").addClass("d-none");
       $("a[btn-submit]").removeClass("d-none");
     }
@@ -50,11 +63,9 @@ $(document).ready(function() {
 
   $("button[btn-play]").click(function() {
     var videoFile =
-      "../assets/vid/" + $("#steps-container").attr("step") + ".mp4";
+      "../assets/vid/" + $("#steps-container").attr("list") + ".mp4";
     $("#video").attr("src", videoFile);
     $("#videoDiv video")[0].load();
     $("#play-vid-modal").modal("show");
   });
 });
-
-// $('input[type=checkbox]:checked').length
