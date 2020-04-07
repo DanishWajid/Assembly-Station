2377ecd861386fafb839571318677849d5df5b9f
abbas <abbas.afzal0810@gmail.com>
2020-04-08 04:24:56 +0500

    Translation main page

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -1,37 +1,61 @@
 $(document).ready(function() {
-  if (localStorage.getItem("language") != null) {
-    var lang = localStorage.getItem("language");
 
-    if (lang == "de") {
-      $("#de").addClass("focus");
-      $("#en").removeClass("focus");
-      $("#start").attr("href", "de/steps.html");
-    }
-    if (lang == "en") {
-      $("#en").addClass("focus");
-      $("#de").removeClass("focus");
-      $("#start").attr("href", "en/steps.html");
-    }
-  }
+    var arrLang = {
+        en: {
+            welcome: "Welcome to the assembly station",
+            select: "Select Language",
+            videoError: "Your browser does not support the video tag.",
+            steps: "Steps",
+        },
+        de: {
+            welcome: "Willkommen in der Montagestation",
+            select: "Sprache auswählen",
+            videoError: "Ihr Browser unterstützt das Video-Tag nicht.",
+            steps: "Schritte",
+        }
+    };
 
-  // Process translation
-  $(function() {
-    $(".translate").click(function() {
-      var lang = localStorage.getItem("language");
-      if (lang == "de") {
-        $("#de").addClass("focus");
-        $("#en").removeClass("focus");
-        $("#start").attr("href", "de/steps.html");
-      }
-      if (lang == "en") {
-        $("#en").addClass("focus");
-        $("#de").removeClass("focus");
-        $("#start").attr("href", "en/steps.html");
-      }
+
+    if (localStorage.getItem("language") != null) {
+        var lang = localStorage.getItem("language");
+
+        if (lang == "de") {
+            $("#de").addClass("focus");
+            $("#en").removeClass("focus");
+            $("#start").attr("href", "de/steps.html");
+        }
+        if (lang == "en") {
+            $("#en").addClass("focus");
+            $("#de").removeClass("focus");
+            $("#start").attr("href", "en/steps.html");
+        }
+        $(".lang").each(function(index, item) {
+            $(this).text(arrLang[lang][$(this).attr("key")]);
+        });
+    }
+
+    // Process translation
+    $(function() {
+        $(".translate").click(function() {
+            var lang = localStorage.getItem("language");
+            if (lang == "de") {
+                $("#de").addClass("focus");
+                $("#en").removeClass("focus");
+                $("#start").attr("href", "de/steps.html");
+            }
+            if (lang == "en") {
+                $("#en").addClass("focus");
+                $("#de").removeClass("focus");
+                $("#start").attr("href", "en/steps.html");
+            }
+
+            $(".lang").each(function(index, item) {
+                $(this).text(arrLang[lang][$(this).attr("key")]);
+            });
+        });
     });
-  });
 });
 
 function setLanguage(lang) {
-  localStorage.setItem("language", lang);
+    localStorage.setItem("language", lang);
 }
